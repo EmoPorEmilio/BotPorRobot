9fd1b7d128552bcbbe5bbb3b13f4d6a9fc2f1eae
Emilio Franceschini <franceschini.emilio@gmail.com>
2022-01-30 21:52:07 -0300

    simple text commands working

---
+++ index.js
@@ -0,0 +1,53 @@
+import tmi from 'tmi.js';
+import {ENV_VARIABLES} from './environment.js';
+import {TEXT_COMMANDS} from './text-commands.js';
+
+// Setup client
+
+const opts = {
+  identity: {
+    username: ENV_VARIABLES.BOT_USERNAME,
+    password: ENV_VARIABLES.OAUTH_TOKEN
+  },
+  channels: [
+    ENV_VARIABLES.CHANNEL_NAME
+  ]
+};
+
+const client = new tmi.client(opts);
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+client.connect();
+
+const sameCommand = (input, command) => {
+    return input.toUpperCase() === command.toUpperCase();
+}
+
+const checkCommandsAndReact = (commandInput, target) => {
+    const foundCommand = TEXT_COMMANDS.find(command => sameCommand(command.name, commandInput))
+    if (foundCommand) {
+        client.say(target, foundCommand.message);
+        console.log(`* Executed ${foundCommand.name} command`);
+    }
+    else {
+        console.log(`* Unknown command ${commandInput}`);
+    }
+};
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandInput = msg.trim();
+
+  // If message is a command and the command is known, let's execute it
+  if(commandInput[0] === '!') {
+    checkCommandsAndReact(commandInput.substring(1), target);
+  }
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
---
+++ text-commands.js
@@ -0,0 +1,22 @@
+const redes = {
+    name: 'redes',
+    message: 'Seguime en mis redes para enterarte de toda novedad! Podés encontrarlas en: https://emoporemilio.uy',
+};
+const twitter = {
+    name: 'twitter',
+    message: 'Seguime en Twitter para leer mis desvaríos en formato texto https://twitter.com/emoporemilio',
+}
+const instagram = {
+    name: 'instagram',
+    message: 'Seguime en Instagram para ver mis desvaríos en formato audiovisual https://instagram.com/emoporemilio',
+};
+const tiktok = {
+    name: 'tiktok',
+    message: 'Seguime en Tiktok para ver mis desvaríos en formato audiovisual https://tiktok.com/@emoporemilio',
+};
+const youtube = {
+    name: 'youtube',
+    message: 'Suscribite a mi cuenta de Youtube para ver transmisiones anteriores y videos random https://youtube.com/emoporemilio',
+};
+
+export const TEXT_COMMANDS = [redes, twitter, instagram, tiktok, youtube];
