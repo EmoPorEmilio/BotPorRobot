7efa7d783379a0e46d5c7aa1a9f3da18c84f58a0
Emilio Franceschini <franceschini.emilio@gmail.com>
2022-03-15 20:47:04 -0300

    extend commands

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 import tmi from 'tmi.js';
-import {ENV_VARIABLES} from './environment.js';
-import {TEXT_COMMANDS, REDES_COMMAND} from './text-commands.js';
+import { ENV_VARIABLES } from './environment.js';
+import { TEXT_COMMANDS, REDES_COMMAND } from './text-commands.js';
 
 // Setup client
 
 const opts = {
   identity: {
     username: ENV_VARIABLES.BOT_USERNAME,
-    password: ENV_VARIABLES.OAUTH_TOKEN
+    password: ENV_VARIABLES.OAUTH_TOKEN,
   },
-  channels: [
-    ENV_VARIABLES.CHANNEL_NAME
-  ]
+  channels: [ENV_VARIABLES.CHANNEL_NAME],
 };
 
 let spamRedesInterval = null;
@@ -21,72 +19,76 @@ client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 client.connect();
 
-
-const sameCommand = (input, command) => {
-    return input.toUpperCase() === command.toUpperCase();
-}
+const sameCommand = (inputNames, command) => {
+  let same = false;
+  inputNames.forEach((input) => {
+    same = same || input.toUpperCase() === command.toUpperCase();
+  });
+  return same;
+};
 
 const commandsList = () => {
-    const reducer = (valorAnterior, valorActual, indice) => {
-        if (indice === 0) {
-            return `${valorAnterior} !${valorActual.name}`
-        }
-        else {
-            return `${valorAnterior}, !${valorActual.name}`
-        }
-    };
-    return TEXT_COMMANDS.reduce(reducer, "Los comandos disponibles son:");
-}
+  const reducer = (valorAnterior, valorActual, indice) => {
+    if (indice === 0) {
+      return `${valorAnterior} !${valorActual.name}`;
+    } else {
+      return `${valorAnterior}, !${valorActual.name}`;
+    }
+  };
+  return TEXT_COMMANDS.reduce(reducer, 'Los comandos disponibles son:');
+};
 
 const checkListCommandAndReact = (commandInput, target) => {
-    if (commandInput === 'comandos') {
-        client.say(target, commandsList());
-        console.log(`* Executed !commandos command`);
-        return true;
-    }
-    else {
-        return false;
-    }
+  if (commandInput === 'comandos') {
+    client.say(target, commandsList());
+    console.log(`* Executed !commandos command`);
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const checkCommandsAndReact = (commandInput, target) => {
-    if (!checkListCommandAndReact(commandInput, target)) {
-        const foundCommand = TEXT_COMMANDS.find(command => sameCommand(command.name, commandInput))
-        if (foundCommand) {
-            client.say(target, foundCommand.message);
-            console.log(`* Executed !${foundCommand.name} command`);
-        }
-        else {
-            console.log(`* Unknown command ${commandInput}`);
-        }
+  if (!checkListCommandAndReact(commandInput, target)) {
+    const foundCommand = TEXT_COMMANDS.find((command) =>
+      sameCommand(command.names, commandInput)
+    );
+    if (foundCommand) {
+      client.say(target, foundCommand.message);
+      console.log(`* Executed !${foundCommand.name} command`);
+    } else {
+      console.log(`* Unknown command ${commandInput}`);
     }
+  }
 };
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+function onMessageHandler(target, context, msg, self) {
+  if (self) {
+    return;
+  } // Ignore messages from the bot
 
   // Remove whitespace from chat message
   const commandInput = msg.trim();
 
   // If message is a command and the command is known, let's execute it
-  if(commandInput[0] === '!') {
+  if (commandInput[0] === '!') {
     checkCommandsAndReact(commandInput.substring(1), target);
   }
 }
 
 const spamRedes = () => {
-    client.say(ENV_VARIABLES.CHANNEL_NAME, REDES_COMMAND.message)
-}
+  client.say(ENV_VARIABLES.CHANNEL_NAME, REDES_COMMAND.message);
+};
 
 const initSpamRedesInterval = () => {
-    spamRedesInterval = setInterval(()=> {
-        spamRedes();
-    }, 60*60*1000);
-}
+  spamRedesInterval = setInterval(() => {
+    spamRedes();
+  }, 60 * 60 * 1000);
+};
 
 // Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-    console.log(`* Connected to ${addr}:${port}`);
-    initSpamRedesInterval();
+function onConnectedHandler(addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+  initSpamRedesInterval();
 }
--- text-commands.js
+++ text-commands.js
@@ -1,22 +1,58 @@
 export const REDES_COMMAND = {
-    name: 'redes',
-    message: 'Seguime en mis redes para enterarte de toda novedad! Podés encontrarlas en: https://emoporemilio.uy',
+  name: 'redes',
+  names: ['redes'],
+  message:
+    'Seguime en mis redes para enterarte de toda novedad! Podés encontrarlas en: https://emoporemilio.uy',
 };
 export const TWITTER_COMMAND = {
-    name: 'twitter',
-    message: 'Seguime en Twitter que tengo Twitter! https://twitter.com/emoporemilio',
-}
+  name: 'twitter',
+  names: ['twitter, tw'],
+  message:
+    'Seguime en Twitter que tengo Twitter! https://twitter.com/emoporemilio',
+};
 export const IG_COMMAND = {
-    name: 'instagram',
-    message: 'Seguime en Instagram que tengo Instagram! https://instagram.com/emoporemilio',
+  name: 'instagram',
+  names: ['instagram, ig'],
+  message:
+    'Seguime en Instagram que tengo Instagram! https://instagram.com/emoporemilio',
 };
 export const TIKTOK_COMMAND = {
-    name: 'tiktok',
-    message: 'Seguime en Tiktok que tengo Tiktok! https://tiktok.com/@emoporemilio',
+  name: 'tiktok',
+  names: ['tiktok'],
+  message:
+    'Seguime en Tiktok que tengo Tiktok! https://tiktok.com/@emoporemilio',
 };
 export const YT_COMMAND = {
-    name: 'youtube',
-    message: 'Suscribite a mi cuenta de Youtube que tengo cuenta de Youtube! https://youtube.com/emoporemilio',
+  name: 'youtube',
+  names: ['youtube, yt'],
+  message:
+    'Suscribite a mi cuenta de Youtube que tengo cuenta de Youtube! https://youtube.com/emoporemilio',
+};
+export const DC_COMMAND = {
+  name: 'discord',
+  names: ['discord, ds'],
+  message:
+    'Sumate al canal de Discord! https://discord.gg/wTbYuszphttps://discord.gg/wTbYuszp',
+};
+export const POKEBRO_COMMAND = {
+  name: 'pokebro',
+  names: ['pokebro'],
+  message:
+    'Mirate el código de la aplicación PokéBro en https://github.com/EmoPorEmilio/pokebro y jugalo en https://emoporemilio.github.io/pokebro/',
+};
+export const GITHUB_COMMAND = {
+  name: 'github',
+  names: ['github'],
+  message:
+    'Seguime en GitHub que tengo GitHub! en https://github.com/EmoPorEmilio',
 };
 
-export const TEXT_COMMANDS = [REDES_COMMAND, TWITTER_COMMAND, IG_COMMAND, TIKTOK_COMMAND, YT_COMMAND];
+export const TEXT_COMMANDS = [
+  REDES_COMMAND,
+  TWITTER_COMMAND,
+  IG_COMMAND,
+  TIKTOK_COMMAND,
+  YT_COMMAND,
+  DC_COMMAND,
+  POKEBRO_COMMAND,
+];
